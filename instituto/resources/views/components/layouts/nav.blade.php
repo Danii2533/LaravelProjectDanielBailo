4d9333174404b0616d846d6446e77c792a4efa2f
Danii2533 <danielbailo2004@gmail.com>
2025-11-28 18:51:39 +0100

    Comiti Inicial

---
+++ instituto/resources/views/components/layouts/nav.blade.php
@@ -0,0 +1,12 @@
+<nav class="bg-nav h-nav flex flex-row justify-start items-center
+space-x-2"> 
+    <button class="">
+        <a href="about">About</a>
+    </button class="">
+    <button>
+        Noticias
+    </button>
+    <button class="">
+        Contactar
+    </button>
+</nav>
